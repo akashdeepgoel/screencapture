5416dc6950d26e10bb991c829a6a01dd98ef1d43
Akashdeep Goel <akashdeep.goel1996@gmail.com>
2015-06-30 18:28:17 +0530

    Feat: Added View Screenshot Option

--- app/src/main/java/com/eecs/screencapture/MainActivity.java
+++ app/src/main/java/com/eecs/screencapture/MainActivity.java
@@ -1,14 +1,45 @@
 package com.eecs.screencapture;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void b1OnClick(View v)
+    {
+        Button button = (Button) v;
+        //button.setText("Hello");
+        Intent intent =  new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivity(intent);
+
+
+    }
+    public boolean doesPackageExist(String targetPackage) {
+
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo info = pm.getPackageInfo(targetPackage, PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
+
+
 }
