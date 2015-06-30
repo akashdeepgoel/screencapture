248fddac02882b1e73140b8ad4949924ea08332b
Karan Desai <karandesai_96@live.com>
2015-06-30 17:28:32 +0530

    Feat: Enabled fullscreen in splash screen, and increased duration.

--- app/src/main/java/com/eecs/screencapture/SplashScreen.java
+++ app/src/main/java/com/eecs/screencapture/SplashScreen.java
@@ -2,18 +2,25 @@ package com.eecs.screencapture;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 
 public class SplashScreen extends Activity {
 
-    private final int SPLASH_DISPLAY_TIME = 1000;
+    private final int SPLASH_DISPLAY_TIME = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         new Handler().postDelayed(new Runnable() {
             @Override
