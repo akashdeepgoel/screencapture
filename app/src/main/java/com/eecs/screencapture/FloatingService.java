7c44a3e73bfa5d96d9815c17cd2cb9471419591d
Karan Desai <karandesai_96@live.com>
2015-07-02 07:43:42 +0530

    Feat: Added dismiss imageView, with its layoutParams.

--- app/src/main/java/com/eecs/screencapture/FloatingService.java
+++ app/src/main/java/com/eecs/screencapture/FloatingService.java
@@ -19,29 +19,41 @@ public class FloatingService extends Service{
 
     private WindowManager               windowManager;
     private ImageView                   floatingHead;
-    private WindowManager.LayoutParams  layoutParams;
+    private WindowManager.LayoutParams  floatingHeadParams;
     private Display                     display;
     private Point                       screenSize = new Point(0,0);
+
+    private ImageView                   dismissBin;
+    private WindowManager.LayoutParams  dismissBinParams;
 
 
     @Override
     public void onCreate() {
         super.onCreate();
-        windowManager           = (WindowManager) getSystemService(WINDOW_SERVICE);
-        display                 = windowManager.getDefaultDisplay();
+        windowManager                   = (WindowManager) getSystemService(WINDOW_SERVICE);
+        display                         = windowManager.getDefaultDisplay();
         display.getSize(screenSize);
 
-        floatingHead            = new ImageView(this);
+        floatingHead                    = new ImageView(this);
         floatingHead.setBackgroundResource(R.mipmap.floating_head);
 
-        layoutParams            = new WindowManager.LayoutParams(
-                (int)Math.sqrt(screenSize.x*screenSize.y/100*Math.PI), (int)Math.sqrt(screenSize.x*screenSize.y/100*Math.PI),
-                WindowManager.LayoutParams.TYPE_PHONE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
+        floatingHeadParams              = new WindowManager.LayoutParams(100, 100, WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
 
-        layoutParams.gravity    = Gravity.TOP | Gravity.START;
-        layoutParams.x          = 0;
-        layoutParams.y          = 100;
+        floatingHeadParams.gravity      = Gravity.TOP | Gravity.LEFT;
+        floatingHeadParams.x            = 0;
+        floatingHeadParams.y            = 100;
+
+
+        dismissBin                      = new ImageView(this);
+        dismissBin.setBackgroundResource(R.mipmap.dismiss_button);
+
+        dismissBinParams                = new WindowManager.LayoutParams(100, 100, WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, PixelFormat.TRANSLUCENT);
+
+        dismissBinParams.gravity        = Gravity.TOP | Gravity.LEFT;
+        dismissBinParams.x              = screenSize.x / 2 - 50;
+        dismissBinParams.y              = screenSize.y + 200;
 
 
         floatingHead.setOnTouchListener(new View.OnTouchListener() {
@@ -54,25 +66,28 @@ public class FloatingService extends Service{
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        initialX = layoutParams.x;
-                        initialY = layoutParams.y;
+                        initialX = floatingHeadParams.x;
+                        initialY = floatingHeadParams.y;
                         initialTouchX = event.getRawX();
                         initialTouchY = event.getRawY();
                         return true;
                     case MotionEvent.ACTION_UP:
+
                         return true;
                     case MotionEvent.ACTION_MOVE:
-                        layoutParams.x = initialX
+                        floatingHeadParams.x = initialX
                                 + (int) (event.getRawX() - initialTouchX);
-                        layoutParams.y = initialY
+                        floatingHeadParams.y = initialY
                                 + (int) (event.getRawY() - initialTouchY);
-                        windowManager.updateViewLayout(floatingHead, layoutParams);
+
+                        windowManager.updateViewLayout(floatingHead, floatingHeadParams);
                         return true;
                 }
                 return false;
             }
         });
-        windowManager.addView(floatingHead, layoutParams);
+        windowManager.addView(floatingHead, floatingHeadParams);
+        windowManager.addView(dismissBin, dismissBinParams);
     }
 
 
@@ -81,6 +96,7 @@ public class FloatingService extends Service{
         super.onDestroy();
         if (floatingHead != null)
             windowManager.removeView(floatingHead);
+            windowManager.removeView(dismissBin);
     }
 
 
